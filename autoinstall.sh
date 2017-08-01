1d67e429c4f70f8f991a3ba575f4f38228ce7597
Miroslav Hájek <mirkousko@gmail.com>
2017-08-01 16:44:46 +0200

    Autoinstall script ready for tests (not safe!)

---
+++ autoinstall.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+SCRIPT_PATH="$( cd "$( dirname "$0" )" && pwd )"
+BACKUP_DIR="$SCRIPT_PATH/backup_Config"
+
+#sudo apt install zsh vim tmux terminator curl git htop -y
+
+# Zálohy
+mkdir $BACKUP_DIR
+cd ~/
+cp .vimrc .bashrc .zshrc .tmux.conf $BACKUP_DIR/
+cp -r ~/.config $BACKUPDIR/
+
+
+# Presun nových skriptov
+mv $SCRIPT_PATH/.vimrc $SCRIPT_PATH/.bashrc $SCRIPT_PATH/.zshrc ~/
+mv $SCRIPT_PATH/.tmux.conf ~/
+mv $SCRIPT_PATH/terminator ~/.config/terminator
+
+# Inštalácia vim pluginov
+cd ~/.vim
+git clone https://github.com/ctrlpvim/ctrlp.vim.git
+git clone https://github.com/scrooloose/nerdtree
+git clone https://github.com/tmhedberg/SimpylFold
+git clone https://github.com/majutsushi/tagbar
+git clone https://github.com/vim-airline/vim-airline
+git clone https://github.com/vim-airline/vim-airline-themes
+git clone https://github.com/edkolev/tmuxline.vim
+git clone https://github.com/nvie/vim-flake8
+git clone https://github.com/vim-scripts/a.vim
+git clone https://github.com/ntpeters/vim-better-whitespace
+
+# Inštalácia zsh pluginov
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
