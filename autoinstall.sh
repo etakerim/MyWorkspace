6ed3b84fecf9223d7582c52355948d27ec08b97d
Miroslav Hájek <mirkousko@gmail.com>
2017-08-06 13:19:46 +0200

    Install script almost ready

--- autoinstall.sh
+++ autoinstall.sh
@@ -1,22 +1,30 @@
-#!/bin/bash
-
+#!/bin/sh
 SCRIPT_PATH="$( cd "$( dirname "$0" )" && pwd )"
-BACKUP_DIR="$SCRIPT_PATH/backup_Config"
+BACKUP_DIR="~/old_config"
 
-#sudo apt install zsh vim tmux terminator curl git htop -y
+sudo apt install zsh vim tmux terminator curl git htop -y
+
+if ! [[ "$SHELL" = "/usr/bin/zsh" ]]; then
+    chsh -s $(which zsh)
+fi
 
 # Zálohy
 mkdir $BACKUP_DIR
 cd ~/
-mv .vimrc .bashrc .zshrc .tmux.conf $BACKUP_DIR/
-mv ~/.config/terminator $BACKUPDIR/
-mv ~/.vim $BACKUP_DIR/
+mv .vimrc .bashrc .zshrc .tmux.conf .tmuxstatusbar \
+    $BACKUP_DIR/  2> /dev/null
+mv ~/.config/terminator/*(D) $BACKUPDIR/terminator/ 2> /dev/null
+mv ~/.vim/*(D) $BACKUP_DIR/.vim/   2> /dev/null
+
+# Inštalácia zsh pluginov
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Presun nových skriptov
-mv $SCRIPT_PATH/.vim  ~/.vim
-mv $SCRIPT_PATH/.vimrc $SCRIPT_PATH/.bashrc $SCRIPT_PATH/.zshrc ~/
-mv $SCRIPT_PATH/.tmux.conf ~/
-mv  $SCRIPT_PATH/terminator ~/.config/terminator
+mv $SCRIPT_PATH/.vim/*(D)  ~/.vim/
+mv $SCRIPT_PATH/.vimrc $SCRIPT_PATH/.bashrc $SCRIPT_PATH/.zshrc \
+   $SCRIPT_PATH/.tmux.conf $SCRIPT_PATH/.tmuxstatusbar ~/
+mv $SCRIPT_PATH/terminator/*(D) ~/.config/terminator/
+mv $SCRIPT_PATH/agnoster.zsh_theme ~/.oh-my-zsh/themes/
 
 # Inštalácia vim pluginov
 cd ~/.vim/bundle
@@ -30,6 +38,3 @@ git clone https://github.com/edkolev/tmuxline.vim
 git clone https://github.com/nvie/vim-flake8
 git clone https://github.com/vim-scripts/a.vim
 git clone https://github.com/ntpeters/vim-better-whitespace
-
-# Inštalácia zsh pluginov
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
